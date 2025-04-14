f805d1c8da26dbdf73c27b452e3a4895afc7672f
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-04-14 13:04:07 -0700

    Create grid of boxes with js DOM manipulation

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+let container = document.querySelector(".container");
+for (i = 0; i< 16; i++) {
+    div = document.createElement("div");
+    div.classList.add("gridDiv");
+    div.textContent = "woah";
+    container.appendChild(div);
+}
