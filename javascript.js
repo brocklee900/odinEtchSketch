d2303578e3b46f8c37b2576e885c85c19780c3b3
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-04-16 11:41:51 -0700

    Create reshapeable grid of hovererable squares

--- javascript.js
+++ javascript.js
@@ -1,21 +1,53 @@
-let container = document.querySelector(".container");
-for (i = 0; i< 16; i++) {
-    div = document.createElement("div");
-    div.classList.add("gridDiv");
-    div.textContent = "woah";
-    container.appendChild(div);
+let container = document.querySelector('.container');
+
+function createGrid(numSquares) {
+    squareSideLength = parseInt(getComputedStyle(container).width) / numSquares;
+    squareSideLength = `${squareSideLength}px`;
+    console.log(squareSideLength);
+
+    for (i = 0; i < numSquares; i++) {
+        for (j = 0; j < numSquares; j++) {
+            div = document.createElement('div');
+            div.classList.add('gridDiv');
+            div.style.width = squareSideLength;
+            div.style.height = squareSideLength;
+            container.appendChild(div);
+        }
+    }
 }
 
-container.addEventListener("mouseover", (e) => {
+container.addEventListener('mouseover', e => {
     target = e.target;
-    if (target.className != "container") {
-        target.classList.toggle("hover");
+    if (target.className != 'container') {
+        target.classList.toggle('hover');
     }
-})
+});
 
-container.addEventListener("mouseout", (e) => {
+container.addEventListener('mouseout', e => {
     target = e.target;
-    if (target.className != "container") {
-        target.classList.toggle("hover");
+    if (target.className != 'container') {
+        target.classList.toggle('hover');
     }
-})
+});
+
+const resize = document.querySelector('#resize');
+resize.addEventListener('click', () => {
+
+    while (container.lastElementChild) {
+        count = 0;
+        while (container.lastElementChild) {
+            count += 1;
+            container.removeChild(container.lastElementChild);
+            if (count > 1000) {
+                break;
+            }
+        }
+    }
+
+    numSquares = +prompt(
+        'Enter the dimensions. (The resulting grid will be inputxinput)'
+    );
+    createGrid(numSquares);
+});
+
+createGrid(10);
